047cce9025b62881242e7ca28869e9e697d0001f
d1rk <d1rk@gmx.de>
2012-02-24 09:19:40 +0100

    Overhaul and cleanup of Analyze
    - removed awful code ;)
    - simplified configuration
    - now uses lithium resources/tmp instead of IDS
    - better usage of include_path
    - provided default configuration
    - configuration can now carry name of session
      configuration to use
    - configuration can now carry name of logging
      configuration to use
    - configuration now allows to adjust format of
      log message
    - minor style-guide fixes

--- extensions/Analyze.php
+++ extensions/Analyze.php
@@ -1,17 +1,17 @@
 <?php
-/**
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
 
 namespace li3_ids\extensions;
 
+use lithium\core\Libraries;
+use lithium\util\String;
 use lithium\storage\Session;
 
 /**
+ * Analyze does the heavy Lifting
  *
- *
- *
+ * It analyzes the Request paramters, like GET and POST
+ * and runs it through the IDS Monitor.
+ * It then can react on certain Impacts.
  */
 class Analyze extends \lithium\core\Adaptable {
 
@@ -35,57 +35,46 @@ class Analyze extends \lithium\core\Adaptable {
 	protected static $_requestInstance = NULL;
 
 	/**
-	 * Runs the Analyzer
+	 * Runs the PHPIDS Analyzer
 	 *
-	 * @param array $params
-	 * @return true
+	 * @param object $request The Request object
+	 * @return boolean true on success, false otherwise
 	 */
-	public static function run($params){
-		//die(print_r(\func_get_args(),true));
-
-		if(\array_key_exists('request', $params)){
-			self::$_requestInstance = $params['request'];
-
-			$request = array(
-				'GET' => $params['request']->query,
-				'POST' => $params['request']->data,
-			);
+	public static function run($request){
+		if(!$request) {
+			return false;
 		}
 
+		self::$_requestInstance = $request;
+
+		$data = array(
+			'GET' => $request->query,
+			'POST' => $request->data,
+		);
 
 		$path = get_include_path();
-		try{
-			//awful code structure is following... :(
+		set_include_path($path.':'.LI3_IDS_LIB_PATH);
 
-			//fetch settings from init/config
-			$idsPath = \dirname(__FILE__) . '/../libraries/' . 'phpids/lib';
+		try {
 
-			if(!$path = \realpath($idsPath)){
-				die('Path to IDS lib does not exists: ' . $idsPath);
-			}else{
-				$idsPath = $path;
-			}
+			require_once  'IDS/Init.php';
+			$init = \IDS_Init::init(LI3_IDS_LIB_PATH . '/IDS/Config/Config.ini.php');
+			$init->config['General']['use_base_path'] = false;
+			$init->config['General']['filter_path'] = LI3_IDS_LIB_PATH . '/IDS/default_filter.xml';
+			$init->config['General']['tmp_path'] = Libraries::get(true, 'path') . '/resources/tmp/';
+			$init->config['Logging']['path'] = Libraries::get(true, 'path') . '/resources/tmp/logs/phpids.log';
+			$init->config['Caching']['path'] = Libraries::get(true, 'path') . '/resources/tmp/cache/default_filter.cache';
 
-			set_include_path($idsPath);
-			require_once 'IDS/Init.php';
-			$init = \IDS_Init::init($idsPath . '/IDS/Config/Config.ini.php');
-			$init->config['General']['base_path'] = $idsPath . '/IDS/';
-			$init->config['General']['use_base_path'] = True;
-
-
-			$ids        = new \IDS_Monitor($request, $init);
-			$result     = $ids->run();
-
-			//re set the include path
-			set_include_path($path);
+			$ids = new \IDS_Monitor($data, $init);
+			$result = $ids->run();
 
 			if($result instanceof \IDS_Report){
-			 static::react($result);
+				static::react($result);
 			}
 		} catch (Exception $e){
 			die($e->getMessage());
 		}
-		return True;
+		return true;
 	}
 
 	/**
@@ -96,32 +85,33 @@ class Analyze extends \lithium\core\Adaptable {
 	 */
 	protected static function react(\IDS_Report $result){
 
-		$currentImpact = $result->getImpact();
+		$current_impact = $result->getImpact();
 		$config = self::config('default');
-		
-		$impact = Session::read('impact',array('name'=>'ids'));
-		$impact += $currentImpact;
-		if(!Session::write('impact', (string)$impact, array('name' => 'ids'))){
-			throw new \Exception('Session write not possible!');
+
+		$impact = Session::read('impact', array('name' => $config['session']));
+		$impact += $current_impact;
+		if(!Session::write('impact', (string)$impact, array('name' => $config['session']))){
+			$msg = sprintf('Impact could not be written to Session "%s"', $config['session']);
+			throw new \Exception($msg);
 		}
-		switch(TRUE) {
+		switch(true) {
 			case $impact >= $config['threshold']['kick']:
 				//IP to Blacklist
-				static::log($result,'kick',$impact);
+				static::log($result, 'kick', $impact);
 				//die('Ihre IP wurde gesperrt!
 				//Total impact:'.$impact.' vs '.$config['threshold']['kick']);
 				break;
 			case $impact >= $config['threshold']['warn']:
 				//warn
-				static::log($result,'warn',$impact);
+				static::log($result, 'warn', $impact);
 				break;
 			case $impact >= $config['threshold']['mail']:
 				//mail
-				static::log($result,'mail',$impact);
+				static::log($result, 'mail', $impact);
 				break;
 			case $impact >= $config['threshold']['log']:
 				//log this
-				static::log($result,'log',$impact);
+				static::log($result, 'log', $impact);
 				break;
 			default:
 				//nothing
@@ -137,29 +127,53 @@ class Analyze extends \lithium\core\Adaptable {
 	 * @param integer $totalImpact
 	 * @return boolean
 	 */
-	protected static function log(\IDS_Report $result,$threshold,$totalImpact){
+	protected static function log(\IDS_Report $result, $threshold, $total_impact){
 		$request = self::$_requestInstance;
-		$params = compact('result','threshold','totalImpact','request');
+		$params = compact('result', 'threshold', 'total_impact', 'request');
 
 		$_classes = static::$_classes;
 
 		return static::_filter(__FUNCTION__, $params, function($self, $params) use ($_classes) {
-			$request = $params['request'];
-			$result = $params['result'];
-			$totalImpact = $params['totalImpact'];
-
 			$config = $self::config('default');
-
-			$threshold = $config['threshold'][$params['threshold']];
-			$ip = $request->env('REMOTE_ADDR');
-
-			$msg = $ip . ' Total Impact: ' . $totalImpact
-			 . ' is raised by: ' . $result->getImpact()
-			 . ' and higher than threshold: ' . $threshold;
-
-			$_classes['logger']::write('warning',$msg,array('name'=>'ids'));
+			$msg = String::insert($config['log_format'], array(
+				'ip' => $params['request']->env('REMOTE_ADDR'),
+				'total_impact' => $params['total_impact'],
+				'impact' => $params['result']->getImpact(),
+				'threshold' => $config['threshold'][$params['threshold']],
+			));
+			$_classes['logger']::write('warning', $msg, array('name' => $config['logger']));
 		});
 	}
+
+	/**
+	 * A stub method called by `_config()` which allows `Adaptable` subclasses to automatically
+	 * assign or auto-generate additional configuration data, once a configuration is first
+	 * accessed. This allows configuration data to be lazy-loaded from adapters or other data
+	 * sources.
+	 *
+	 * @param string $name The name of the configuration which is being accessed. This is the key
+	 *               name containing the specific set of configuration passed into `config()`.
+	 * @param array $config Contains the configuration assigned to `$name`. If this configuration is
+	 *              segregated by environment, then this will contain the configuration for the
+	 *              current environment.
+	 * @return array Returns the final array of settings for the given named configuration.
+	 */
+	protected static function _initConfig($name, $config) {
+		$defaults = array(
+			'adapter' => null,
+			'session' => 'default', // name of Session config to use
+			'logger' => 'ids', // name of Logger config to use
+			'log_format' => '{:ip} - Total impact "{:total_impact}" is raised by "{:impact}" and higher than threshold "{:threshold}"',
+			'threshold' => array(
+				'log'  => 3,
+				'mail' => 9,
+				'warn' => 27,
+				'kick' => 81,
+			),
+		);
+		return (array) $config + $defaults;
+	}
+
 }
 
 ?>
