9a413db0fbb4f09b4468fc347f96c2b292004030
d1rk <d1rk@gmx.de>
2013-09-06 16:33:07 +0200

    renamed / fixed AnalyzerTest

---
+++ tests/cases/security/AnalyzerTest.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace li3_ids\tests\cases\security;
+
+use li3_ids\security\Analyzer;
+
+class AnalyzerTest extends \lithium\test\Unit {
+
+	public function setUp() {}
+
+	public function tearDown() {}
+
+	public function testRun() {}
+}
+
+?>
